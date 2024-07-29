382613a4322b30eba9cf6e69fe69bd1277fe8766
AprilXu8168 <april.xu8168@gmail.com>
2024-07-29 14:05:05 +1000

    More about db interaction

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 if (builder.Environment.IsDevelopment())
@@ -8,15 +10,23 @@ if (builder.Environment.IsDevelopment())
     builder.Services.AddDbContext<MvcMovieContext>(options =>
         options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
 }
-else
-{
-    builder.Services.AddDbContext<MvcMovieContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
-}
+// else
+// {
+//     builder.Services.AddDbContext<MvcMovieContext>(options =>
+//         options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
+// }
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Add the seed initializer
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;    
+
+    SeedData.Initialize(services);
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
