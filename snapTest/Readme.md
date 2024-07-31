6f9d415a2129df8dd9abbcec75f17cfcadf2d26d
AprilXu8168 <april.xu8168@gmail.com>
2024-07-31 12:48:45 +1000

    snapshot test example

--- snapTest/Readme.md
+++ snapTest/Readme.md
@@ -1,1 +1,11 @@
-### Test with snapshot testing 
+# JEST Snapshot testing
+
+## Running tests 
+
+```bash
+npm run test
+```
+## Running the application
+```bash
+npm run start
+```
---
+++ snapTest/src/App.test.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
+import Counter from "./Counter";
+
+
+describe("Jest Snapshot testing suite", () => {
+  it("Matches DOM Snapshot", () => {
+    const domTree = renderer.create(<App />).toJSON();
+    expect(domTree).toMatchSnapshot();
+  });
+  
+  it("Should render 3 after three increments", () => {
+    const component = renderer.create(<Counter />);
+    component.getInstance().increment();
+    component.getInstance().increment();
+    component.getInstance().increment();
+    expect(component.toJSON()).toMatchSnapshot();
+  })
+});
